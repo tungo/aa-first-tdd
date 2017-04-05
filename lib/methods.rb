0189ce7bdea61286aca99238aeb51143f8296943
Tu Ngo <tungo8810@gmail.com>
2017-04-05 11:44:45 -0700

    Add two_sum pending case

--- lib/methods.rb
+++ lib/methods.rb
@@ -1,3 +1,15 @@
 def my_uniq(arr)
-  
+  raise "Argument is not an array" unless arr.is_a?(Array)
+
+  result = []
+
+  arr.each { |el| result << el unless result.include?(el) }
+
+  result
+end
+
+class Array
+  def two_sum
+    
+  end
 end
--- spec/methods_spec.rb
+++ spec/methods_spec.rb
@@ -7,19 +7,28 @@ describe "#my_uniq" do
   end
 
   it "raise error if argument is not an array" do
-    expect{my_uniq(2)}.to raise_error("Argument is not an array")
+    expect { my_uniq(2) }.to raise_error("Argument is not an array")
   end
 
   it "return array if argument is an unique array" do
     expect(my_uniq([1, 2, 3])).to eq([1, 2, 3])
   end
+
   it "remove duplicate elements" do
     expect(my_uniq([1, 2, 1, 3, 3])).to eq([1, 2, 3])
   end
 
+  let(:arr) { [2, 2, 1, 3, 1] }
+  let(:r) { my_uniq(arr) }
+
   it "doesn't modify the original array" do
-    arr = [2, 2, 1, 3, 1]
-    r = my_uniq(arr)
-    expect(arr).to be_not(arr)
+    expect(r).not_to be(arr)
   end
+end
+
+describe "Array#two_sum" do
+  it "return empty array if array is empty"
+  it "doesn't modify the original array"
+  it "return empty array if no pairs"
+  it "returns pairs in order"
 end
